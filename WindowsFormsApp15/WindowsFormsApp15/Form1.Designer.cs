a107d2861ade524915bc9951adadb3e984586f19
AlisaTarasova <76028528+AlisaTarasova@users.noreply.github.com>
2022-05-26 16:40:38 +0500

    Add files via upload

---
+++ WindowsFormsApp15/WindowsFormsApp15/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WindowsFormsApp15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 143);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(457, 292);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 25);
+            this.textBox2.MaximumSize = new System.Drawing.Size(700, 700);
+            this.textBox2.MinimumSize = new System.Drawing.Size(50, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(563, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(698, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Запись\r\nв файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(680, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выбранный файл: -";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Выбрать файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(698, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 50);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Чтение\r\nиз файла";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(490, 159);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(298, 276);
+            this.listBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(487, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Демонстрация записи в файл";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp15/WindowsFormsApp15/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {        
+        static public Random rnd = new Random();
+        static public string[] words = { "год", "человек","время","дело","жизнь","день","рука",
+            "работа","место","вопрос","лицо","глаз","страна","друг","сторона","дом","случай",
+            "ребенок","система","вид","конец","отношение","проблема","земля","решение"};
+
+        string fileName = null;
+
+        public string FileName
+        {
+            get
+            {
+                return fileName;
+            }
+            set
+            {
+                fileName = value;
+                MyInvoke(label1, () =>
+                {
+                    if (fileName == null)
+                        label1.Text = "Файл не выбран";
+                    else
+                        label1.Text =  "Выбранный файл: " + fileName;
+                });
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+        private void WriteToFile()
+        {
+            try
+            {
+                Stream writer = new FileStream(FileName, FileMode.Append, FileAccess.Write, FileShare.Read);
+                StreamWriter myWriter = new StreamWriter(writer);
+
+                for (int i = 0; i < 30; i++)
+                {
+                    string str = words[rnd.Next(words.Length)];
+                    MyInvoke(listBox2, () =>
+                    {
+                        listBox2.Items.Add(str);
+                    });
+                    myWriter.WriteLine(str);
+                    myWriter.Flush();
+                    Thread.Sleep(3000);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("В файл уже производит запись!");
+            }
+           
+        }
+
+        private void ReadInFile()
+        {
+            Stream reader = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader myReader = new StreamReader(reader);
+            string str;
+            while ((str = myReader.ReadLine()) != null)
+            {
+               MyInvoke(listBox1, () =>
+               {
+                    listBox1.Items.Add(str);
+               });               
+               Thread.Sleep(3000);
+            }         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+        }  
+
+        private void MyInvoke(Control item, Action action)
+        {
+            if (item.InvokeRequired)
+            {
+                item.Invoke(new Action(action));
+                return;
+            }
+            action();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (FileName == null)
+            {
+                MessageBox.Show("Файл не выбран!");
+                return;
+            }           
+            
+            Thread thread1 = new Thread(WriteToFile);
+            thread1.Start();
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            FileName = openFileDialog1.FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (FileName == null)
+            {
+                MessageBox.Show("Файл не выбран!");
+                return;
+            }
+
+            Thread thread2 = new Thread(ReadInFile);
+            thread2.Start();          
+        }
+    }
+}
+
